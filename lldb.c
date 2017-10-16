041b730643e353d25148dc8e55ce878ca1f45eee
YoYoMagicRoss <trickstertournaments@gmail.com>
2017-10-15 19:17:28 -0700

    first commit

---
+++ lldb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int one, two, sum;
+    
+    printf("Enter two integers: ");
+
+    // Two integers entered by user is stored using scanf() function
+    scanf("%d %d", &one, &two);
+
+    // sum of two numbers in stored in variable sum
+sum = one + two;
+
+    // Displays sum      
+    printf("%d + %d = %d", one, two, sum);
+
+    return 0;
+}
+
+// ^^ the program ^^
+// Spoiler, if you're reading this. The mistake is super lame.
